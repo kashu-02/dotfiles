e1a0061286377ef0bb4aaec1545acd1d8ec3b9e1
kashu <64694079+kashu-02@users.noreply.github.com>
2025-02-04 02:22:02 +0900

    home-manager modularize

--- users/lab-pc-home.nix
+++ users/lab-pc-home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./packages/common-linux.nix
+  ];
+  
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "kashu";
@@ -14,51 +18,6 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  nixpkgs.config.allowUnfree = true;
-  home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-    google-chrome
-    slack
-    discord
-    _1password-gui
-    thunderbird
-    vlc
-    vscode
-    pcmanfm
-    nodejs
-    xarchiver
-    gimp
-    libreoffice
-    bottom
-    neofetch
-    arandr
-    pavucontrol
-    evince
-    tmux
-    htop
-    jetbrains-toolbox
-    xclip
-    maim
-    wine
-  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
---
+++ users/packages/common-linux.nix
@@ -0,0 +1,55 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./git.nix
+    ./nvim.nix
+  ];
+
+  
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    google-chrome
+    slack
+    discord
+    _1password-gui
+    thunderbird
+    vlc
+    vscode
+    nodejs
+    xarchiver
+    gimp
+    libreoffice
+    bottom
+    fastfetch
+    arandr
+    pavucontrol
+    evince
+    tmux
+    htop
+    jetbrains-toolbox
+    xclip
+    maim
+    wine
+  ];
+}
