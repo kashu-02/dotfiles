5c18413d2c4ee1411e3d62aa33fff899696f9e2b
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2025-02-04 03:02:03 +0900

    add ssh

---
+++ users/packages/ssh.nix
@@ -0,0 +1,12 @@
+{ pkgs, ...} : {
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/github";
+      };
+    };
+  };
+}
