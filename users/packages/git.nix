84c1fb00cd412be6ed954c738cfd2e82066037f9
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2025-02-04 03:01:48 +0900

    fix git

--- users/packages/git.nix
+++ users/packages/git.nix
@@ -5,7 +5,18 @@
     enable = true;
     userName = "kashu-02";
     userEmail = "64694079+kashu-02@users.noreply.github.com";
-    signing.key = "0F4484A65691E216";
-    signing.signByDefault = true;
+    aliases = {
+      sw = "switch";
+      cm = "commit -m";
+      cc = "commit --amend";
+      st = "status -uno";
+      pu = "push origin";
+      pl = "pull origin";
+    };
+    ignores = [".idea/"];
+    signing = {
+      key = "0F4484A65691E216";
+      signByDefault = true;
+    };
   };
 }
