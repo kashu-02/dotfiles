06f831e832b5b64ae8aa423d0539954707628456
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2025-02-04 05:44:08 +0900

    tmux

--- users/packages/tmux.nix
+++ users/packages/tmux.nix
@@ -6,5 +6,15 @@
     baseIndex = 1;
     keyMode = "vi";
     clock24 = true;
+    historyLimit = 100000;
+    mouse = true;
+    extraConfig = ''
+      set -g default-terminal 'screen-256color'
+      set -g terminal-overrides 'xterm:colors=256'
+      set-option -g renumber-windows on
+      set-option -g status-position top
+      set -g status-fg 'brightwhite'
+      set -g status-bg 'colour238'
+    '';
   };
 }
