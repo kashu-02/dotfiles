93d8d7c942d02eae282c51441ff500a8e61dcd0e
kashu-02 <64694079+kashu-02@users.noreply.github.com>
2025-03-21 03:06:13 +0900

    fix vscode

---
+++ hosts/l390-laptop/l390-laptop-configuration.nix
@@ -0,0 +1,200 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./l390-laptop-hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking = {
+	hostName = "l390-laptop";
+	# wireless.iwd.enable = true;
+	networkmanager = {
+		enable = true;
+		# wifi.backend = "iwd";
+	};
+  };
+  programs.nm-applet.enable = true;
+
+  # Bluetooth Settings 
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+ 
+ 
+  # Brightness
+  programs.light.enable = true;
+
+  # Input devices
+  hardware.trackpoint = {
+	enable = true;
+	emulateWheel = true;
+	speed = 1;
+	sensitivity = 1;
+	device = "Elan TrackPoint";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Asia/Tokyo";
+  time.hardwareClockInLocalTime = true;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ja_JP.UTF-8";
+    LC_IDENTIFICATION = "ja_JP.UTF-8";
+    LC_MEASUREMENT = "ja_JP.UTF-8";
+    LC_MONETARY = "ja_JP.UTF-8";
+    LC_NAME = "ja_JP.UTF-8";
+    LC_NUMERIC = "ja_JP.UTF-8";
+    LC_PAPER = "ja_JP.UTF-8";
+    LC_TELEPHONE = "ja_JP.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+  
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [fcitx5-mozc fcitx5-skk fcitx5-gtk libsForQt5.fcitx5-qt];
+  };
+  fonts.packages = with pkgs; [
+  	ipafont
+  	noto-fonts
+	noto-fonts-cjk-sans
+	noto-fonts-emoji
+        jetbrains-mono
+  ];
+
+
+  environment.pathsToLink = ["/libexec"];
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    xkb = {
+      variant = "";
+      options = "ctrl:nocaps";
+      layout = "us";
+    };
+    desktopManager = {
+	xterm.enable = false;
+    };
+   windowManager.i3 = {
+  	enable = true;
+    	extraPackages = with pkgs;[
+  	  dmenu
+	  i3status
+	  i3lock
+	];
+    };
+  };
+
+  services.displayManager = {
+    defaultSession = "none+i3";
+  };
+ 
+  services.libinput = {
+    enable = true;
+    touchpad.disableWhileTyping = true;
+  };
+
+  # Configure console keymap
+  console.keyMap = "us";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.shun = {
+  #   isNormalUser = true;
+  #   description = "shun";
+  #   shell = pkgs.zsh;
+  #   extraGroups = [ "networkmanager" "wheel" "audio" "video" "docker"];
+  #   packages = with pkgs; [];
+  # };
+  users.users.kashu = {
+    isNormalUser = true;
+    description = "kashu";
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" "audio" "video" "docker"];
+    packages = with pkgs; [];
+  };
+  nix.settings.allowed-users = [ "shun" "kashu"];
+
+    # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  programs.zsh.enable = true;
+  environment.shells = with pkgs; [ zsh ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    emacs
+    wget
+    openssl
+    zip
+    unzip
+    traceroute
+    dig
+  ];
+
+  # Nix-ld
+  programs.nix-ld.enable = true;
+
+  # Cloudflare-WARP
+  services.cloudflare-warp = {
+    enable = true;
+  };
+
+  # Docker
+  virtualisation.docker.enable = true;
+ 
+  # nix storag optimization
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 90d";
+  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+}
--- users/packages/ssh.nix
+++ users/packages/ssh.nix
@@ -1,12 +1,34 @@
 { pkgs, ...} : {
   programs.ssh = {
     enable = true;
-    matchBlocks = {
-      "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/github";
-      };
+    compression = true;
+    serverAliveInterval = 10;
+    matchBlocks."github.com" = {
+      hostname = "github.com";
+      identitiesOnly = true;
+      identityFile = "~/.ssh/github";
+      user = "git";
+    };
+    matchBlocks."dev-nix" = {
+      hostname = "home.kashu.dev";
+      identityFile = "~/.ssh/dev-nix";
+      port = 56754;
+      user = "kashu";
+    };
+    matchBlocks."lab-pc" = {
+      hostname = "192.168.50.100";
+      identityFile = "~/.ssh/lab-pc";
+      user = "kashu";
+      proxyJump = "dev-nix";
+    };
+    matchBlocks."u-aizu" = {
+      hostname = "sshgate.u-aizu.ac.jp";
+      identityFile = "~/.ssh/u-aizu";
+      user = "s1300071";
+    };
+    matchBlocks."linsv" = {
+      hostname = "linsv.u-aizu.ac.jp";
+      proxyJump = "u-aizu";
     };
   };
 }
